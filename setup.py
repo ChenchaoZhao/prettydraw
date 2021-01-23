58bdb13bf2f3771b73759c45a991ee05f34fe40f
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-01-23 14:21:49 -0500

    typo

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version="0.0.0",
     description="Draw pretty annotations using PIL.",
     py_modules=["prettydraw"],
-    package_dir={'', "src"}
+    package_dir={'': "src"},
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
