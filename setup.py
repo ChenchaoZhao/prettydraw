8e99b106faf8e2c35e94f00178e1817cf9464061
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-01-24 13:15:09 -0500

    reformat

--- setup.py
+++ setup.py
@@ -4,21 +4,15 @@ from setuptools import setup, find_packages
 with open("README.md", 'r') as f:
     long_description = f.read()
 
-print(find_packages())
-
-setup(
-    name="prettydraw",
-    version="0.0.0",
-    author="Chenchao Zhao",
-    author_email="chenchao.zhao@gmail.com",
-    url="https://github.com/ChenchaoZhao/prettydraw",
-    description="Draw pretty annotations using PIL.",
-    py_modules=["prettydraw"],
-    package_dir={'': "prettydraw"},
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=[
-        "pillow",
-        "numpy"
-    ]
-)
+setup(name="prettydraw",
+      version="0.0.0",
+      author="Chenchao Zhao",
+      author_email="chenchao.zhao@gmail.com",
+      url="https://github.com/ChenchaoZhao/prettydraw",
+      description="Draw pretty annotations using PIL.",
+      py_modules=["prettydraw"],
+      package_dir={'': "prettydraw"},
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      install_requires=["pillow", "numpy"],
+      license="MIT")
